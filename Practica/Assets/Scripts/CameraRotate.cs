272b30c998a54d683a6f42b3bb60df60660cd361
Kevin <alu0100789461@ull.edu.es>
2016-10-14 11:33:55 +0100

    Add project

---
+++ Practica/Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+	public float XSensitivity = 2f;
+	public float YSensitivity = 2f;
+	public bool lockCursor = true;
+
+	private Quaternion m_CharacterTargetRot;
+	private Quaternion m_CameraTargetRot;
+	private bool m_cursorIsLocked = true;
+
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+		m_CharacterTargetRot = player.localRotation;
+		m_CameraTargetRot = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float yRot = Input.GetAxis ("Mouse X") * XSensitivity;
+		float xRot = Input.GetAxis ("Mouse Y") * YSensitivity;
+
+		m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+		m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+
+		player.localRotation = m_CharacterTargetRot;
+		transform.localRotation = m_CameraTargetRot;
+
+		UpdateCursorLock();
+	}
+
+	public void UpdateCursorLock()
+	{
+		//if the user set "lockCursor" we check & properly lock the cursos
+		if (lockCursor)
+			InternalLockUpdate();
+	}
+
+	private void InternalLockUpdate()
+	{
+		if(Input.GetKeyUp(KeyCode.Escape))
+		{
+			m_cursorIsLocked = false;
+		}
+		else if(Input.GetMouseButtonUp(0))
+		{
+			m_cursorIsLocked = true;
+		}
+
+		if (m_cursorIsLocked)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		else if (!m_cursorIsLocked)
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+}
---
+++ Practica/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControl : MonoBehaviour {
+	// Canvas update
+	public delegate void ChangeInfoBalls();
+	public static event ChangeInfoBalls InfoBalls;
+	// Color balls
+	public delegate void ChangeColorBalls(Color color);
+	public static event ChangeColorBalls ColorBalls;
+
+	private Color cyan = Color.cyan;
+	private Color red = Color.gray;
+	private Color white = Color.white;
+	private Color pink = Color.magenta;
+	private Color grey = Color.grey;
+	private Color green = Color.green;
+
+	public void changeColorBalls() {
+		InfoBalls ();	// Call event
+		//GameObject[] balls = GameObject.FindGameObjectsWithTag ("Ball");
+
+		Color colorToApply = new Color();
+		int number = Random.Range (0, 6);
+		switch (number) {
+		case 0:
+			{
+				colorToApply = cyan;
+			}
+			break;
+		case 1:
+			{
+				colorToApply = red;
+			}
+			break;
+		case 2:
+			{
+				colorToApply = white;
+			}
+			break;
+		case 3:
+			{
+				colorToApply = pink;
+			}
+			break;
+		case 4:
+			{
+				colorToApply = grey;
+			}
+			break;
+		case 5:
+			{
+				colorToApply = green;
+			}
+			break;
+		}
+
+		ColorBalls (colorToApply);
+
+		/*foreach (GameObject selected in balls) {
+			Renderer rend = selected.GetComponent<Renderer> ();
+			rend.material.color = colorToApply;
+			selected.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 200, 0));
+		}*/
+	}
+}
